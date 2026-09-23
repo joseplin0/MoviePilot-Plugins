c68a897bb0d39141175adbebe6b058eafbda22bf
joseplin <joseplin@foxmail.com>
2026-09-23 14:38:09 +0800

    chore[subscribeautosort]: 升级插件版本至1.4.4
    将SubscribeAutoSort插件的plugin_version从1.4.3更新为1.4.4。

--- plugins/subscribeautosort/__init__.py
+++ plugins/subscribeautosort/__init__.py
@@ -25,7 +25,7 @@ class SubscribeAutoSort(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/joseplin0/MoviePilot-Plugins/main/icons/s_order.png"
     # 插件版本
-    plugin_version = "1.4.3"
+    plugin_version = "1.4.4"
     # 插件作者
     plugin_author = "joseplin0"
     # 作者主页
